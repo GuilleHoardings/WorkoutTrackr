768e4937d9d4f93348a49e692bdf4a48d62f247c
Guillermo Hoardings <guille.hoardings@gmail.com>
2024-01-01 17:01:14 +0100

    Working, with activty tracker included

--- pushuptracker.js
+++ pushuptracker.js
@@ -14,6 +14,7 @@ if (localStorage.getItem("pushUpsData")) {
     pushUpsData = JSON.parse(localStorage.getItem("pushUpsData"));
     pushUpsData.forEach(data => addRowToTable(data));
     createOrUpdateCharts();
+    createActivityChart();
 }
 
 // Listen to the form's submit event
@@ -22,21 +23,22 @@ pushUpForm.addEventListener("submit", (e) => {
 
     // Get push-up count from the input field
     const pushUps = parseInt(pushUpsInput.value);
-    const currentDate = new Date();
+    const newEntryTime = new Date();
 
-    // Remove the time part from the current date to compare only the date part
-    currentDate.setHours(0, 0, 0, 0);
+    // Remove the time part from the submitted entry time to compare only the date part
+    const entryDate = new Date(newEntryTime);
 
     // Check if an entry with the current date already exists in the array
-    const existingEntry = pushUpsData.find(data => new Date(data.date).setHours(0, 0, 0, 0) === currentDate.getTime());
+    const existingEntry = pushUpsData.find(data => new Date(data.date).setHours(0, 0, 0, 0) === entryDate.setHours(0, 0, 0, 0));
 
     if (existingEntry) {
-        // If an entry exists, update the push-up count
+        // If an entry exists, update the push-up count and timeBetweenFirstAndLast
         existingEntry.pushUps += pushUps;
+        existingEntry.timeBetweenFirstAndLast = Math.round((newEntryTime.getTime() - new Date(existingEntry.date).getTime()) / 60000);
     } else {
         // Create a new data object and push it into the array
         const pushUpData = {
-            date: currentDate,
+            date: newEntryTime,
             pushUps: pushUps,
             timeBetweenFirstAndLast: 0
         };
@@ -47,9 +49,9 @@ pushUpForm.addEventListener("submit", (e) => {
     localStorage.setItem("pushUpsData", JSON.stringify(pushUpsData));
 
     // Clear and repopulate the table rows
-    pushUpsData.forEach(data => {
+    while (pushUpTable.rows.length > 1) {
         pushUpTable.deleteRow(-1);
-    });
+    }
 
     pushUpsData.forEach(data => {
         addRowToTable(data);
@@ -57,19 +59,31 @@ pushUpForm.addEventListener("submit", (e) => {
 
     // Update the charts
     createOrUpdateCharts();
-
 });
 
+function createLongFormattedDate(date) {
+    return new Intl.DateTimeFormat('es-ES', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        year: '2-digit',
+    }).format(date);
+}
+
+function createShortFormattedDate(date) {
+    return new Intl.DateTimeFormat().format(date);
+}
 
 function addRowToTable(data) {
-    const row = pushUpTable.insertRow(-1);
+    const row = pushUpTable.insertRow(1);
     const dateCell = row.insertCell(0);
     const pushUpsCell = row.insertCell(1);
     const timeCell = row.insertCell(2);
 
     // Format and display the data in the table
-    const formattedDate = new Intl.DateTimeFormat().format(new Date(data.date));
-    dateCell.innerHTML = formattedDate;
+    dateCell.innerHTML = createLongFormattedDate(new Date(data.date));
     pushUpsCell.innerHTML = data.pushUps;
     timeCell.innerHTML = data.timeBetweenFirstAndLast + ' minutes';
 }
@@ -77,11 +91,11 @@ function addRowToTable(data) {
 function createOrUpdateCharts() {
     // Check if the charts already exist, if they do, update the data
     if (chart1 && chart2) {
-        chart1.data.labels = pushUpsData.map(data => new Intl.DateTimeFormat().format(new Date(data.date)));
+        chart1.data.labels = pushUpsData.map(data => createShortFormattedDate(new Date(data.date)));
         chart1.data.datasets[0].data = pushUpsData.map(data => data.pushUps);
         chart1.update();
 
-        chart2.data.labels = pushUpsData.map(data => new Intl.DateTimeFormat().format(new Date(data.date)));
+        chart2.data.labels = pushUpsData.map(data => createShortFormattedDate(new Date(data.date)));
         chart2.data.datasets[0].data = pushUpsData.map(data => data.pushUps / data.timeBetweenFirstAndLast);
         chart2.update();
     } else {
@@ -89,7 +103,7 @@ function createOrUpdateCharts() {
         chart1 = new Chart(pushUpsChart, {
             type: "bar",
             data: {
-                labels: pushUpsData.map(data => new Intl.DateTimeFormat().format(new Date(data.date))),
+                labels: pushUpsData.map(data => createShortFormattedDate(new Date(data.date))),
                 datasets: [{
                     label: "Push ups",
                     data: pushUpsData.map(data => data.pushUps),
@@ -110,7 +124,7 @@ function createOrUpdateCharts() {
         chart2 = new Chart(pushUpsPerMinuteChart, {
             type: "line",
             data: {
-                labels: pushUpsData.map(data => new Intl.DateTimeFormat().format(new Date(data.date))),
+                labels: pushUpsData.map(data => createShortFormattedDate(new Date(data.date))),
                 datasets: [{
                     label: "Push ups per minute",
                     data: pushUpsData.map(data => data.pushUps / data.timeBetweenFirstAndLast),
@@ -128,4 +142,100 @@ function createOrUpdateCharts() {
             }
         });
     }
+}
+
+function createActivityChart() {
+    var data = JSON.parse(localStorage.getItem('pushUpsData'));
+
+    // Get the range of years from pushUpsData
+    var minYear = new Date(data[0].date).getFullYear();
+    var maxYear = new Date(data[data.length - 1].date).getFullYear();
+    var numYears = maxYear - minYear + 1;
+
+    var canvas = document.getElementById('activity');
+    var ctx = canvas.getContext('2d');
+
+    var cellSize = 10;
+    var padding = 2;
+    var yearPadding = 10;
+    var yearTextSize = 10;
+    var monthTextSize = 15;
+    var yearWidth = 53 * cellSize + padding * 54;
+    var yearHeight = 7 * cellSize + padding * 8 + yearTextSize + monthTextSize;
+
+    canvas.width = yearWidth;
+    canvas.height = numYears * yearHeight + yearPadding * 2;
+
+    // Define an array of month names each two months
+    var monthNames = ['Jan', 'Mar', 'May', 'Jul', 'Sep', 'Nov']
+
+    // Draw the years in reverse order
+    for (var absYear = maxYear; absYear >= minYear; absYear--) {
+        // Print the year
+        var year = absYear - minYear;
+        ctx.fillStyle = '#888';
+        ctx.font = 'bold 10px sans-serif';
+        yYearStart = year * yearHeight + (year + 1) * yearPadding + yearTextSize + monthTextSize;
+        ctx.fillText(absYear, padding, yYearStart - monthTextSize);
+
+        // Draw the grid
+        for (var i = 0; i < 53; i++) {
+            // Draw the month names
+            if (i % 9 === 0) {
+                var month = Math.floor(i / 9);
+                ctx.fillStyle = '#888';
+                ctx.font = 'bold 10px sans-serif';
+                ctx.fillText(monthNames[month], i * cellSize + padding * (i + 1), yYearStart);
+            }
+
+            ctx.fillStyle = '#ddd';
+            for (var j = 0; j < 7; j++) {
+                var x = i * cellSize + padding * (i + 1);
+                var y = j * cellSize + padding * (j + 1) + yYearStart;
+                ctx.fillRect(x, y, cellSize, cellSize);
+            }
+        }
+
+    }
+    // Draw the push-ups
+    for (var i = 0; i < data.length; i++) {
+        var date = new Date(data[i].date);
+        var year = date.getFullYear();
+        var day = date.getDay();
+        // Make the week start in Monday
+        if (day === 0) {
+            day = 6;
+        } else {
+            day--;
+        }
+        var week = getWeekNumber(date)[1] - 1;
+        var x = week * cellSize + padding * (week + 1);
+        var y = day * cellSize + padding * (day + 1) + (year - minYear) * yearHeight + (year - minYear + 1) * yearPadding + yearTextSize + monthTextSize;
+        var color = getGreenShade(data[i].pushUps);
+        // console.log(`year: ${year} day: ${day}, week: ${week}, x: ${x}, y: ${y}, color: ${color}`); // eslint-disable-line no-console
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, cellSize, cellSize);
+    }
+}
+
+function getWeekNumber(date) {
+    var d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    var dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return [d.getUTCFullYear(), Math.ceil((((d - yearStart) / 86400000) + 1) / 7)];
+}
+
+function getGreenShade(pushUps) {
+    // Return green, but it make it proportional to the push-up count, with a
+    // maximum of 120. The hihger the push-up count, the darker the shade of
+    // green. Using github's color scale.
+    var maxPushUps = 120;
+    var color = '#ebedf0';
+    if (pushUps > 0) {
+        var shade = Math.round((pushUps / maxPushUps) * 3);
+        color = ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b'][shade];
+    }
+    return color;
+
 }
